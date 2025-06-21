9497eb3de645b32660a62f09c1027e2faa8a2e76
tannt <129242298+k3sojuuu@users.noreply.github.com>
2025-06-21 10:57:09 +0700

    tannt

--- frontend-ADFD/lib/service/place_service.dart
+++ frontend-ADFD/lib/service/place_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import '../models/place.dart';
 
 class PlaceService {
-  static const String apiUrl = 'http://10.0.2.2:8080/api/getAllPlace'; // 10.0.2.2 là localhost cho Android Emulator
+  static const String apiUrl = 'http://10.0.2.2:8080/api/getAllPlace';
 
   static Future<List<Place>> fetchPlaces() async {
     final response = await http.get(Uri.parse(apiUrl));
